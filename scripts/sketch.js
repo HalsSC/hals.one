ca5111e493254bdec84ce819e00ac01099ad005c
HalsSC <me@hals.one>
2022-03-03 13:47:31 +0900

    canvas resize

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,29 +1,33 @@
+var canvas;
+
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(document.documentElement.scrollWidth, document.documentElement.scrollHeight);
     background(0);
 }
 
 function setup(){
-    let canvas=createCanvas(windowWidth, windowHeight);
-    canvas.position(0,0);
-    canvas.style('z-index','-1');
+    canvas = createCanvas(document.documentElement.scrollWidth, document.documentElement.scrollHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    canvasSetup();
+    setInterval(()=>{
+        background(0);
+    }, 15000);
+}
+
+function canvasSetup(){
     background(0);
     noStroke();
-    setInterval(canvasReset, 15000);
 }
 
 function draw(){
     frameRate(10);
-    let rand=parseInt(random(245,255));
-    if(rand%6==0)      fill(rand, 255-rand, 255-rand, 75);
-    else if(rand%6==1) fill(255-rand, rand, 255-rand, 75);
-    else if(rand%6==2) fill(255-rand, 255-rand, rand, 75);
-    else if(rand%6==3) fill(rand, rand, 255-rand, 75);
-    else if(rand%6==4) fill(rand, 255-rand, rand, 75);
-    else if(rand%6==5) fill(255-rand, rand, rand, 75);
-    rect(random(-width,width), random(-height,height), rand-170, rand-170);
-}
-
-function canvasReset(){
-    background(0);
+    let rand = parseInt(random(245, 255));
+    if(rand%6 == 0)      fill(rand, 255-rand, 255-rand, 75);
+    else if(rand%6 == 1) fill(255-rand, rand, 255-rand, 75);
+    else if(rand%6 == 2) fill(255-rand, 255-rand, rand, 75);
+    else if(rand%6 == 3) fill(rand, rand, 255-rand, 75);
+    else if(rand%6 == 4) fill(rand, 255-rand, rand, 75);
+    else if(rand%6 == 5) fill(255-rand, rand, rand, 75);
+    rect(random(-width, width), random(-height, height), rand - 170, rand - 170);
 }
